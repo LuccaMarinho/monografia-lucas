9bbeb96d1dc094d4195e3d3c87528fc4772f8381
Lucas Marinho <lnmarinho@hotmail.com>
2024-12-23 13:18:05 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -155,14 +155,7 @@ def find_closest_songs_weighted(G, song_A_id, song_B_id, X, sp, dfa):
 
     ranked_songs.sort(key=lambda x: x[1])
 
-    # Ensure the end song is the last song (if it exists in ranked_songs)
-    try:
-        ranked_songs.remove((song_B_id, distances_B[song_B_id]))
-    except ValueError:
-        pass
-    ranked_songs.append((song_B_id, float('inf')))  # End song with lowest rank
-
-    return [song_id for song_id, score in ranked_songs[:X+2]]
+    return [song_id for song_id, score in ranked_songs[:X]]
 
 def create_spotify_playlist(user_id, playlist_name, track_ids, sp):
     try:
@@ -202,6 +195,8 @@ def main():
                 
                 try:
                     closest_songs = find_closest_songs_weighted(G, start_track_id, end_track_id, num_songs - 2, sp, dfa)
+                    closest_songs.insert(0, start_track_id)  
+                    closest_songs.append(end_track_id)
                     track_names = [get_track_info(track_id, sp)[0] for track_id in closest_songs]
                     st.write('Playlist Tracks:', track_names)
                     playlist_id = create_spotify_playlist(user_id, 'Generated Playlist', closest_songs, sp)
